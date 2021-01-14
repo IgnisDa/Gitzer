b7ece00647bf1599100f4f6e318a191397eb5206
IgnisDa <ignisda2001@gmail.com>
2021-01-14 17:35:03 +0000

    feat(gitzer/gitzer/urls.py): add GrahQLView for testing

--- gitzer/gitzer/urls.py
+++ gitzer/gitzer/urls.py
@@ -1,6 +1,10 @@
+from director import views
 from django.contrib import admin
 from django.urls import path
 
+from gitzer.api.graphql_config import schema
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("graphql/", views.GitzerGraphQLView.as_view(schema=schema), name="graphql"),
 ]
