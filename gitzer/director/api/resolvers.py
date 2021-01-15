821504648f38edce1bcef2f1fd951cda9a0c303a
IgnisDa <ignisda2001@gmail.com>
2021-01-15 10:42:17 +0000

    feat(gitzer/director/api): add resolvers and schema for checking existence of a directory

--- gitzer/director/api/resolvers.py
+++ gitzer/director/api/resolvers.py
@@ -16,3 +16,19 @@ def status(*_, directory="."):
         "modified_files": modified_files,
         "staged_files": staged_files,
     }
+
+
+@query.field("existence")
+@convert_kwargs_to_snake_case
+def existence(*_, directory="."):
+    # print(info.context)
+    try:
+        git.Repo(directory)
+        return {
+            "exists": True,
+            "message": "The specified repository was found successfully",
+        }
+    except git.exc.InvalidGitRepositoryError:
+        return {"exists": False, "message": "The path is not a valid git repository"}
+    except git.exc.NoSuchPathError:
+        return {"exists": False, "message": "The path specified is not valid"}
