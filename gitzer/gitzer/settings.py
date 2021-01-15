bf2cbb73fea57368ae1345c953f498562bd37b81
IgnisDa <ignisda2001@gmail.com>
2021-01-15 10:42:37 +0000

    feat(gitzer/gitzer/settings.py): add cors headers settings

--- gitzer/gitzer/settings.py
+++ gitzer/gitzer/settings.py
@@ -27,11 +27,13 @@ INSTALLED_APPS = [
     # django addons
     "django_extensions",
     "ariadne.contrib.django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
