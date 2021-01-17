96cb712df51159648606de1c1b06551c67e7e2de
IgnisDa <ignisda2001@gmail.com>
2021-01-17 05:19:11 +0000

    feat(gitzer/director/apps.py): watchdog to watch changes to *.graphql files

--- gitzer/director/apps.py
+++ gitzer/director/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.utils.autoreload import autoreload_started
+
+
+def schema_watchdog(sender, **kwargs):
+    sender.watch_dir(settings.BASE_DIR, "**/*.graphql")
 
 
 class DirectorConfig(AppConfig):
-    name = 'director'
+    name = "director"
+
+    def ready(self):
+        autoreload_started.connect(schema_watchdog)
