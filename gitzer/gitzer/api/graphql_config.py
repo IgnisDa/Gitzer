bec5489c1488962aea60f75ee33ef65bb9670d19
IgnisDa <ignisda2001@gmail.com>
2021-01-17 07:23:43 +0000

    feat(gitzer/gitzer/api/graphql_config.py): schema files now loaded dynamically

--- gitzer/gitzer/api/graphql_config.py
+++ gitzer/gitzer/api/graphql_config.py
@@ -1,14 +1,18 @@
+import glob
+
 from ariadne import (
     load_schema_from_path,
     make_executable_schema,
     snake_case_fallback_resolvers,
 )
 from director.api import resolvers as director_resolvers
+from django.conf import settings
 
 type_defs = [
-    load_schema_from_path("./gitzer/api/schema.graphql"),
-    load_schema_from_path("./director/api/schema.graphql"),
+    load_schema_from_path(f)
+    for f in glob.glob(str(settings.BASE_DIR / "*" / "api" / "schema" / "*.graphql"))
 ]
+
 schema = make_executable_schema(
     type_defs,
     [director_resolvers.query, director_resolvers.mutation],
