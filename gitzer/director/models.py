f5bfb0b6e0c6a265f21c84caf3e8d40c0866a366
IgnisDa <ignisda2001@gmail.com>
2021-01-17 05:19:45 +0000

    feat(gitzer/director): add Model repository to store repo information

--- gitzer/director/models.py
+++ gitzer/director/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Repository(models.Model):
+    path = models.CharField(
+        help_text=_("The absolute path to the local git repository."), max_length=500
+    )
+    created_on = models.DateTimeField(
+        help_text=_("The date and time this repository was logged on."),
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"Repository in {self.path}"
