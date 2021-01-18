2c080b87404a777584fbe09610fa7ab2dd072c79
IgnisDa <ignisda2001@gmail.com>
2021-01-18 13:11:40 +0000

    feat(gitzer-frontend/pages/status.vue): prettify table

--- gitzer-frontend/pages/status.vue
+++ gitzer-frontend/pages/status.vue
@@ -1,145 +1,138 @@
 <template>
-  <div class="min-h-screen w-full flex flex-col">
-    <!-- <client-only>
-      <div v-if="loading">Loading your repository...</div>
-    </client-only> -->
-    <div class="container mx-auto">
-      <div class="flex flex-wrap overflow-hidden">
-        <div
-          class="max-h-96 w-full md:w-1/3 overflow-auto border-l border-b border-black relative"
-        >
-          <div class="text-center bg-yellow-200 sticky top-0">
-            Untracked Files
-          </div>
-          <div>
-            <div v-if="untrackedFiles.length !== 0">
+  <div
+    class="min-h-screen flex w-full from-gray-900 bg-gradient-to-l to-gray-800"
+  >
+    <div class="container mx-auto flex flex-col">
+      <div class="flex-grow flex">
+        <div class="flex flex-wrap flex-grow my-auto">
+          <div
+            class="w-full md:w-1/3 rounded-lg p-2 max-h-80 overflow-auto relative"
+          >
+            <div class="px-1 shadow-lg">
               <div
-                v-for="(file, index) in untrackedFiles"
-                :key="index"
-                class="p-2 flex justify-between"
-                :class="index % 2 ? 'bg-blue-200' : 'bg-blue-300'"
+                class="text-3xl text-center font-serif text-blue-800 sticky top-0 bg-black p-3 rounded-3xl"
               >
-                <div>
-                  <div>
-                    {{ file.name | getFilename }}
+                Untracked Files
+              </div>
+              <div v-if="untrackedFiles.length !== 0">
+                <div
+                  v-for="(file, index) in untrackedFiles"
+                  :key="index"
+                  class="p-2 flex justify-between my-1 hover:bg-indigo-900 rounded-xl"
+                  :class="index % 2 ? 'bg-gray-900' : 'bg-gray-700'"
+                >
+                  <div class="font-mono justify-items-center">
+                    <div class="text-white">
+                      {{ file.name | getFilename }}
+                    </div>
+                    <div class="ml-2 text-sm text-gray-300">
+                      {{ file.name | getFileRoot }}
+                    </div>
                   </div>
-                  <div class="ml-2 text-sm">
-                    {{ file.name | getFileRoot }}
+                  <div class="flex items-center">
+                    <button
+                      class="mx-1 shadow-inner rounded-full p-1"
+                      @click="
+                        stageFileAction({
+                          filename: file.name,
+                          directory: $route.query.directory,
+                        })
+                      "
+                    >
+                      <FontAwesomeIcon class="h-7" :icon="['fas', 'plus']" />
+                    </button>
                   </div>
-                </div>
-                <div class="flex items-center">
-                  <button
-                    class="mx-1 shadow-inner rounded-full p-1"
-                    @click="
-                      stageFileAction({
-                        filename: file.name,
-                        directory: $route.query.directory,
-                      })
-                    "
-                  >
-                    <FontAwesomeIcon class="h-7" :icon="['fas', 'plus']" />
-                  </button>
                 </div>
               </div>
             </div>
-            <div v-else>No files</div>
           </div>
-        </div>
-
-        <div
-          class="max-h-96 w-full md:w-1/3 overflow-auto border-l border-b border-black relative"
-        >
-          <div class="text-center bg-yellow-200 sticky top-0">
-            Modified Files
-          </div>
-          <div>
-            <div v-if="modifiedFiles.length !== 0">
+          <div
+            class="w-full md:w-1/3 rounded-lg p-2 max-h-80 overflow-auto relative"
+          >
+            <div class="px-1 shadow-lg">
               <div
-                v-for="(file, index) in modifiedFiles"
-                :key="index"
-                class="p-2 flex justify-between"
-                :class="index % 2 ? 'bg-blue-200' : 'bg-blue-300'"
+                class="text-3xl text-center font-serif text-blue-800 sticky top-0 bg-black p-3 rounded-3xl"
               >
-                <div>
-                  <div>
-                    {{ file.name | getFilename }}
+                Modified Files
+              </div>
+              <div v-if="modifiedFiles.length !== 0">
+                <div
+                  v-for="(file, index) in modifiedFiles"
+                  :key="index"
+                  class="p-2 flex justify-between my-1 hover:bg-indigo-900 rounded-xl"
+                  :class="index % 2 ? 'bg-gray-900' : 'bg-gray-700'"
+                >
+                  <div class="font-mono justify-items-center">
+                    <div class="text-white">
+                      {{ file.name | getFilename }}
+                    </div>
+                    <div class="ml-2 text-sm text-gray-300">
+                      {{ file.name | getFileRoot }}
+                    </div>
                   </div>
-                  <div class="ml-2 text-sm">
-                    {{ file.name | getFileRoot }}
+                  <div class="flex items-center">
+                    <button
+                      class="mx-1 shadow-inner rounded-full p-1"
+                      @click="
+                        discardFileChangeAction({
+                          filename: file.name,
+                          directory: $route.query.directory,
+                        })
+                      "
+                    >
+                      <FontAwesomeIcon class="h-7" :icon="['fas', 'trash']" />
+                    </button>
                   </div>
-                </div>
-                <div class="flex items-center">
-                  <button
-                    class="mx-1 shadow-inner rounded-full p-1"
-                    @click="
-                      stageFileAction({
-                        filename: file.name,
-                        directory: $route.query.directory,
-                      })
-                    "
-                  >
-                    <FontAwesomeIcon class="h-7" :icon="['fas', 'plus']" />
-                  </button>
-                  <button
-                    class="mx-1 shadow-inner rounded-full p-1"
-                    @click="
-                      discardFileChangeAction({
-                        filename: file.name,
-                        directory: $route.query.directory,
-                      })
-                    "
-                  >
-                    <FontAwesomeIcon class="h-7" :icon="['fas', 'trash']" />
-                  </button>
                 </div>
               </div>
             </div>
-            <div v-else>No files</div>
           </div>
-        </div>
-
-        <div
-          class="max-h-96 w-full md:w-1/3 overflow-auto border-l border-r border-b border-black relative"
-        >
-          <div class="text-center bg-yellow-200 sticky top-0">Staged Files</div>
-          <div>
-            <div v-if="stagedFiles.length !== 0">
+          <div
+            class="w-full md:w-1/3 rounded-lg p-2 max-h-80 overflow-auto relative"
+          >
+            <div class="px-1 shadow-lg">
               <div
-                v-for="(file, index) in stagedFiles"
-                :key="index"
-                class="p-2 flex justify-between"
-                :class="index % 2 ? 'bg-blue-200' : 'bg-blue-300'"
+                class="text-3xl text-center font-serif text-blue-800 sticky top-0 bg-black p-3 rounded-3xl"
               >
-                <div>
-                  <div>
-                    {{ file.name | getFilename }}
+                Staged Files
+              </div>
+              <div v-if="stagedFiles.length !== 0">
+                <div
+                  v-for="(file, index) in stagedFiles"
+                  :key="index"
+                  class="p-2 flex justify-between my-1 hover:bg-indigo-900 rounded-xl"
+                  :class="index % 2 ? 'bg-gray-900' : 'bg-gray-700'"
+                >
+                  <div class="font-mono justify-items-center">
+                    <div class="text-white">
+                      {{ file.name | getFilename }}
+                    </div>
+                    <div class="ml-2 text-sm text-gray-300">
+                      {{ file.name | getFileRoot }}
+                    </div>
                   </div>
-                  <div class="ml-2 text-sm">
-                    {{ file.name | getFileRoot }}
+                  <div class="flex items-center">
+                    <button
+                      class="mx-1 shadow-inner rounded-full p-1"
+                      @click="
+                        unstageFileAction({
+                          filename: file.name,
+                          directory: $route.query.directory,
+                        })
+                      "
+                    >
+                      <FontAwesomeIcon class="h-7" :icon="['fas', 'minus']" />
+                    </button>
                   </div>
-                </div>
-                <div class="flex items-center">
-                  <button
-                    class="mx-1 shadow-inner rounded-full p-1"
-                    @click="
-                      unstageFileAction({
-                        filename: file.name,
-                        directory: $route.query.directory,
-                      })
-                    "
-                  >
-                    <FontAwesomeIcon class="h-7" :icon="['fas', 'minus']" />
-                  </button>
                 </div>
               </div>
             </div>
-            <div v-else>No files</div>
           </div>
         </div>
       </div>
-    </div>
-    <div>
-      <CommitMessageForm />
+      <div class="flex flex-wrap my-auto flex-grow">
+        <CommitMessageForm />
+      </div>
     </div>
   </div>
 </template>
@@ -193,3 +186,22 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #d62929;
+}
+</style>
