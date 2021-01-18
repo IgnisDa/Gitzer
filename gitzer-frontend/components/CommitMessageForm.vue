6ff3644388dd86b8e081eaa75f3a9484c78a33a8
IgnisDa <ignisda2001@gmail.com>
2021-01-18 11:24:49 +0000

    feat(gitzer-frontend/components/CommitMessageForm.vue): add button to change scope

--- gitzer-frontend/components/CommitMessageForm.vue
+++ gitzer-frontend/components/CommitMessageForm.vue
@@ -1,8 +1,6 @@
 <template>
-  <form
-    class="bg-green-200 container mx-auto h-64"
-    @submit.prevent="commitChange(formData)"
-  >
+  <form class="container mx-auto h-64" @submit.prevent="commitChange(formData)">
+    <div>Commit</div>
     <select v-model="formData.commitType">
       <option value="">Please select one</option>
       <option>fix</option>
@@ -15,7 +13,13 @@
       <option>build</option>
       <option>ci</option>
     </select>
-    <input v-model="formData.commitScope" type="text" name="scope" required />
+    <input
+      v-model="formData.commitScope"
+      class="w-full"
+      type="text"
+      name="scope"
+      required
+    />
     <input
       v-model="formData.commitSummary"
       type="text"
@@ -24,25 +28,35 @@
     />
     <input v-model="formData.commitInfo" type="text" name="commit-info" />
     <button type="submit">Commit</button>
+    {{ commitScopeGetter }}
+    <button @click="setScope()">Set Scope</button>
   </form>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   data: () => ({
     formData: {
       commitType: 'fix',
       commitScope: 'views.py',
-      commitSummary: 'summary for this change',
-      commitInfo: 'this is a complete info of the change',
+      commitSummary: '',
+      commitInfo: '',
     },
   }),
+  computed: {
+    ...mapGetters({
+      commitScopeGetter: 'repository/commitScope',
+    }),
+  },
   methods: {
     ...mapActions({
       commitChangeAction: 'repository/commitChange',
     }),
+    setScope() {
+      this.formData.commitScope = this.commitScopeGetter
+    },
     commitChange(formData) {
       let commitMessage = `${formData.commitType}(${formData.commitScope}): ${formData.commitSummary}`
       if (formData.commitInfo) {
