2684c372f125f506d2d42a5984202ce2ecd99cce
IgnisDa <ignisda2001@gmail.com>
2021-01-18 04:18:18 +0000

    feat(gitzer-frontend/pages/index.vue): add watcher to automatically change existence status

--- gitzer-frontend/pages/index.vue
+++ gitzer-frontend/pages/index.vue
@@ -21,23 +21,6 @@
                   class="flex-shrink text-black font-serif font-thin flex-grow leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-r-none px-3 relative"
                   placeholder="Repository"
                 />
-                <button type="submit" class="flex -mr-px">
-                  <span
-                    class="flex items-center leading-normal bg-grey-lighter rounded rounded-l-none border border-l-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm"
-                    ><svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      class="h-8 w-8 fill-current text-white"
-                      fill="currentColor"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                </button>
               </form>
             </div>
             <div class="mx-2 mt-2 text-gray-500">
@@ -94,6 +77,11 @@ export default {
   }),
   head() {
     return { title: 'Gitzer' }
+  },
+  watch: {
+    directory() {
+      this.checkRepository()
+    },
   },
   mounted() {
     this.checkRepository()
