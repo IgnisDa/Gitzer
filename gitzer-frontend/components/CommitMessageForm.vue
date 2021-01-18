dfb243228d46b9834c570cde441afe9a2679b88b
IgnisDa <ignisda2001@gmail.com>
2021-01-18 14:30:13 +0000

    fix(gitzer-frontend/components/CommitMessageForm.vue): make commit message better

--- gitzer-frontend/components/CommitMessageForm.vue
+++ gitzer-frontend/components/CommitMessageForm.vue
@@ -2,15 +2,18 @@
   <div class="flex-grow">
     <form class="m-4" @submit.prevent="commitChange(formData)">
       <div
-        class="text-3xl text-center font-serif text-blue-800 bg-black p-5 rounded-3xl"
+        class="text-3xl text-center font-serif text-blue-800 bg-black p-5 rounded-3xl ring ring-green-600"
       >
         Commit
       </div>
 
-      <div class="flex text-sm sm:text-base md:text-lg flex-wrap">
+      <div class="flex text-sm sm:text-base md:text-lg flex-wrap mt-4">
         <div class="w-3/5 flex flex-col">
-          <div class="mx-2 text-blue-600">Type of commit*:</div>
-          <select v-model="formData.commitType" class="m-2">
+          <div class="mx-2 text-purple-200">Type of commit*:</div>
+          <select
+            v-model="formData.commitType"
+            class="m-3 ring ring-purple-600 rounded-lg"
+          >
             <option value="">Choose</option>
             <option>fix</option>
             <option>feat</option>
@@ -22,42 +25,46 @@
             <option>build</option>
             <option>ci</option>
           </select>
-          <div class="mx-2 text-blue-600">Scope of commit*:</div>
-          <div class="m-2 flex">
+          <div class="mx-2 text-purple-200">Scope of commit*:</div>
+          <div class="m-3 flex">
             <input
               v-model="formData.commitScope"
               type="text"
-              class="w-1/2"
+              class="w-1/2 ring ring-purple-600 rounded-lg"
               name="scope"
-              required
             />
             <button
               type="button"
-              class="w-1/2 ml-2 bg-purple-900"
+              class="w-1/2 ml-2 bg-purple-900 ring ring-purple-600 rounded-lg"
               @click="setScope()"
             >
               Automatic Scope
             </button>
           </div>
-          <div class="mx-2 text-blue-600">Summary of commit*:</div>
+          <div class="mx-2 text-purple-200">Summary of commit*:</div>
           <input
             v-model="formData.commitSummary"
             type="text"
-            class="m-2"
+            class="m-3 ring ring-purple-600 rounded-lg"
             name="summary"
             required
           />
         </div>
         <div class="flex flex-col w-2/5">
-          <div class="mx-2 text-blue-600">Additional information:</div>
+          <div class="mx-2 text-purple-200">Additional information:</div>
           <textarea
             v-model="formData.commitInfo"
-            class="h-full resize-none m-2"
+            class="h-full resize-none m-3 ring ring-purple-600 rounded-lg"
             type="text"
             name="commit-info"
           />
         </div>
-        <button class="w-full mx-2 bg-purple-900" type="submit">Commit</button>
+        <button
+          class="w-full mx-2 bg-purple-900 ring ring-purple-600 rounded-lg"
+          type="submit"
+        >
+          Commit
+        </button>
       </div>
     </form>
   </div>
@@ -88,7 +95,11 @@ export default {
       this.formData.commitScope = this.commitScopeGetter
     },
     commitChange(formData) {
-      let commitMessage = `${formData.commitType}(${formData.commitScope}): ${formData.commitSummary}`
+      let commitMessage = `${formData.commitType}`
+      if (formData.commitScope) {
+        commitMessage += `(${formData.commitScope})`
+      }
+      commitMessage += `: ${formData.commitSummary}`
       if (formData.commitInfo) {
         commitMessage += `\n\n${formData.commitInfo}`
       }
