55b4c669ce92931845eb46200b11e42e1fc34822
IgnisDa <ignisda2001@gmail.com>
2021-01-23 11:43:28 +0550

    fix(apollo-config.js): remove unnecessary `or`

--- gitzer-frontend/plugins/apollo-config.js
+++ gitzer-frontend/plugins/apollo-config.js
@@ -1,6 +1,5 @@
 export default (context) => {
   return {
-    httpEndpoint:
-      context.$config.backendUrl || 'http://127.0.0.1:8534/graphql/',
+    httpEndpoint: context.$config.backendUrl,
   }
 }
