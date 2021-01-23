03cc5e610d8b9947ddc5bc425352c07518be6dde
IgnisDa <ignisda2001@gmail.com>
2021-01-23 15:54:28 +0550

    feat(nuxt.config.js): add description

--- gitzer-frontend/nuxt.config.js
+++ gitzer-frontend/nuxt.config.js
@@ -5,7 +5,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'The frontend of Gitzer - a web based client for simple git operations',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
