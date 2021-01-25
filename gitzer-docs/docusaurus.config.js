dbc058229063dc615da4858c63b07e441b2060f2
IgnisDa <ignisda2001@gmail.com>
2021-01-25 15:13:03 +0550

    fix(docusaurus.config.js): remove footer

--- gitzer-docs/docusaurus.config.js
+++ gitzer-docs/docusaurus.config.js
@@ -9,6 +9,7 @@ module.exports = {
   organizationName: "IgnisDa",
   projectName: "Gitzer",
   themeConfig: {
+    defaultMode: "dark",
     navbar: {
       title: "Gitzer",
       logo: {
@@ -31,51 +32,6 @@ module.exports = {
     },
     footer: {
       style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Style Guide",
-              to: "docs/",
-            },
-            {
-              label: "Second Doc",
-              to: "docs/doc2/",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "blog",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
-            },
-          ],
-        },
-      ],
       copyright: `Copyright © ${new Date().getFullYear()} Diptesh Choudhuri, Built with Docusaurus.`,
     },
   },
