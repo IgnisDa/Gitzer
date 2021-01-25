0629007a6e5e94abbc96d4bed200b04be50afefe
IgnisDa <ignisda2001@gmail.com>
2021-01-25 15:13:17 +0550

    feat(sidebars.js): correct id's supplied

--- gitzer-docs/sidebars.js
+++ gitzer-docs/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
-  someSidebar: {
-    Gitzer: ["doc1", "doc2", "doc3"],
-    // Features: ["mdx"],
+  docs: {
+    "Using Gitzer": ["installing", "configuration", "doc3"],
+    Development: ["mdx"],
   },
 };
