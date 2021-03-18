b95fb57f8ce87e13edd72458fe4245b78f0d8bcf
IgnisDa <ignisda2001@gmail.com>
2021-03-18 14:10:56 +0550

    feat(views.py): log mutations and queries

--- gitzer/director/views.py
+++ gitzer/director/views.py
@@ -1,5 +1,16 @@
 from ariadne.contrib.django.views import GraphQLView
+from django.conf import settings
+from django.http import HttpRequest
 
 
 class GitzerGraphQLView(GraphQLView):
-    pass
+    def post(self, request: HttpRequest, *args, **kwargs):
+        data = self.extract_data_from_json_request(request)
+        query = data.get("query")
+        if "typename" in query:
+            with open(settings.GITZER_LOGGING_FILE, "a") as f:
+                if "mutation" in query:
+                    f.write("Performed mutation\n")
+                else:
+                    f.write("Performed query\n")
+        return super(GitzerGraphQLView, self).post(request, *args, **kwargs)
