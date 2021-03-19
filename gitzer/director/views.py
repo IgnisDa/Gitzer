fe3a862c476dab495336730d6e7b56ef0dd7ecec
IgnisDa <ignisda2001@gmail.com>
2021-03-19 10:59:01 +0550

    fix(views.py): show time in logs

--- gitzer/director/views.py
+++ gitzer/director/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from ariadne.contrib.django.views import GraphQLView
 from django.conf import settings
 from django.http import HttpRequest
@@ -7,10 +9,12 @@ class GitzerGraphQLView(GraphQLView):
     def post(self, request: HttpRequest, *args, **kwargs):
         data = self.extract_data_from_json_request(request)
         query = data.get("query")
+        now = datetime.now()
         if "typename" in query:
             with open(settings.GITZER_LOGGING_FILE, "a") as f:
                 if "mutation" in query:
-                    f.write("Performed mutation\n")
+                    write = f"{now}: Performed mutation\n"
                 else:
-                    f.write("Performed query\n")
+                    write = f"{now}: Performed query\n"
+                f.write(write)
         return super(GitzerGraphQLView, self).post(request, *args, **kwargs)
