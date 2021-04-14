34c6ed5490d712a128af545a06eacb6531c02ed0
IgnisDa <ignisda2001@gmail.com>
2021-04-14 13:29:14 +0550

    feat(TimesIcon.vue): icon to display X icon on alerts

---
+++ gitzer-frontend/components/icons/TimesIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <FontAwesomeIcon
+      :icon="['fas', 'times']"
+      :class="classes"
+    ></FontAwesomeIcon>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: {
+      type: String,
+      default: 'h-8 w-8',
+    },
+  },
+}
+</script>
