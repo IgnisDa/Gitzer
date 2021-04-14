6d26e09e37c20fef0f676dbf7618e1a0dc5cf3e8
IgnisDa <ignisda2001@gmail.com>
2021-04-14 13:29:46 +0550

    feat(fontawesome.js): import icons

--- gitzer-frontend/plugins/fontawesome.js
+++ gitzer-frontend/plugins/fontawesome.js
@@ -9,6 +9,8 @@ import {
   faFolderOpen,
   faHome,
   faArrowUp,
+  faCheck,
+  faTimes,
 } from '@fortawesome/free-solid-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -16,7 +18,17 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faPlus, faMinus, faTrash, faHome, faSync, faFolderOpen, faArrowUp)
+library.add(
+  faPlus,
+  faMinus,
+  faTrash,
+  faHome,
+  faSync,
+  faFolderOpen,
+  faArrowUp,
+  faCheck,
+  faTimes
+)
 
 // Register the component globally
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
