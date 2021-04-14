104ed1530b7acf52af0b3ff44ed51e65101ce084
IgnisDa <ignisda2001@gmail.com>
2021-04-14 13:30:01 +0550

    feat(GitFilesArea.vue): component to display a single column

---
+++ gitzer-frontend/components/GitFilesArea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full md:w-1/3">
+    <div>{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
