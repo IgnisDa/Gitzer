9977fd118d3bb0399aa306373da804515fad853e
IgnisDa <ignisda2001@gmail.com>
2021-04-14 13:30:22 +0550

    feat(nuxt.config.js): add ignisnents plugin

--- gitzer-frontend/nuxt.config.js
+++ gitzer-frontend/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/fontawesome.js'],
+  plugins: ['~/plugins/fontawesome.js', '~/plugins/ignisnents.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
