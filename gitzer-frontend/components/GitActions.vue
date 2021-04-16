6239016883c5e66a024d267194dcf95ad463e494
IgnisDa <ignisda2001@gmail.com>
2021-04-16 15:00:57 +0550

    feat(GitActions.vue): component to handle performing git actions

--- gitzer-frontend/components/GitActions.vue
+++ gitzer-frontend/components/GitActions.vue
@@ -1,30 +1,36 @@
 <template>
-  <div class="flex justify-around items-center text-lime-100">
-    <NuxtLink :to="{ name: 'index' }" title="Home">
-      <FontAwesomeIcon
-        class="h-12 hover:text-red-600 transition duration-300 ease-in-out"
-        :icon="['fas', 'home']"
-      />
+  <div class="flex items-center justify-around text-lime-100">
+    <NuxtLink :to="{ name: 'index' }" title="Home" class="text-[12px]">
+      <IgIcon
+        name="home"
+        class="w-8 h-8 transition duration-300 ease-in-out lg:h-14 lg:w-14 md:h-12 md:w-12 sm:h-10 sm:w-10 hover:text-red-600 text-lime-200"
+        no-size
+        no-color
+      ></IgIcon>
     </NuxtLink>
     <button
       class="appearance-none focus:outline-none"
       title="Refresh status"
       @click="fetchStatusAction({ directory: $route.query.directory })"
     >
-      <FontAwesomeIcon
-        class="h-12 hover:text-red-600 transition duration-300 ease-in-out"
-        :icon="['fas', 'sync']"
-      />
+      <IgIcon
+        name="refresh-cw"
+        class="w-8 h-8 transition duration-300 ease-in-out lg:h-14 lg:w-14 md:h-12 md:w-12 sm:h-10 sm:w-10 hover:text-red-600 text-lime-200"
+        no-size
+        no-color
+      ></IgIcon>
     </button>
     <button
       class="appearance-none focus:outline-none"
       :title="`Push '${branch}' branch to 'origin'`"
       @click="pushToOriginAction({ directory: $route.query.directory })"
     >
-      <FontAwesomeIcon
-        class="h-12 hover:text-red-600 transition duration-300 ease-in-out"
-        :icon="['fas', 'arrow-up']"
-      />
+      <IgIcon
+        name="chevrons-up"
+        class="w-8 h-8 transition duration-300 ease-in-out lg:h-14 lg:w-14 md:h-12 md:w-12 sm:h-10 sm:w-10 hover:text-red-600 text-lime-200"
+        no-size
+        no-color
+      ></IgIcon>
     </button>
   </div>
 </template>
@@ -33,6 +39,7 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
+  components: {},
   computed: {
     ...mapState({
       branch: (state) => state.repository.branch,
