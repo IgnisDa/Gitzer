9894864ff8e738127d78b73e62f1bb8db3917716
IgnisDa <ignisda2001@gmail.com>
2021-04-17 14:01:30 +0550

    feat(nuxt.config.js, hotkeys.js): add hotkey plugin

--- gitzer-frontend/nuxt.config.js
+++ gitzer-frontend/nuxt.config.js
@@ -19,7 +19,11 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/fontawesome.js', '~/plugins/ignisnents.js'],
+  plugins: [
+    '~/plugins/fontawesome.js',
+    '~/plugins/ignisnents.js',
+    { src: '~/plugins/hotkeys.js', mode: 'client' },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
