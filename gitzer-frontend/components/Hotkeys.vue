a313c18a2ea6170d8ddc1818c1017d930cc679e8
IgnisDa <ignisda2001@gmail.com>
2021-04-17 14:01:39 +0550

    feat(Hotkeys.vue): component to handle hotkeys

---
+++ gitzer-frontend/components/Hotkeys.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hidden">
+    <span v-hotkey="keymap"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    }
+  },
+  computed: {
+    keymap() {
+      return {
+        'ctrl+enter': this.commit,
+      }
+    },
+  },
+  methods: {
+    commit() {
+      this.$emit('submitForm')
+    },
+  },
+}
+</script>
