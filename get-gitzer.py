7cc528e771dfaafed2362748b258336622e75253
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 04:15:49 +0000

    Adding tarfile member sanitization to extractall()

--- get-gitzer.py
+++ get-gitzer.py
@@ -94,7 +94,26 @@ class Installer:
         colored_print("INFO", f"Installing Gitzer to {str(GITZER_PATH)}...")
         with tarfile.open(gitzer_tar, "r:gz") as tar_file:
             temporary_dir = self.gitzer_temp_directory()
-            tar_file.extractall(temporary_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_file, temporary_dir)
             os.makedirs(GITZER_HOME, exist_ok=True)
             shutil.move(temporary_dir / "build", GITZER_PATH)
         self.set_git_alias()
